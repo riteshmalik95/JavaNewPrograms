feee68c7f929122630328994c681c5a1c9423d6a
74489ritesh <riteshmalikmiet@gmail.com>
2025-10-04 13:15:33 +0530

    Add new Java Code

---
+++ src/main/java/com/sumitsircodes/MySolutions/SwapWithThird.java
@@ -0,0 +1,12 @@
+package com.sumitsircodes.MySolutions;
+
+public class SwapWithThird {
+    public static void main(String[] args) {
+        int a=20,b=30,temp;
+        System.out.println("Before Swap: a="+a+" b="+b);
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After Swap: a="+a+" b="+b);
+    }
+}
