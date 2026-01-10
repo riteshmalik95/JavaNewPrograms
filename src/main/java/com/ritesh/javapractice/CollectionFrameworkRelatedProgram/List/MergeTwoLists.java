09de44aaad1dc38df9ab3f0105719c2a3818d051
riteshmalik95 <riteshmalikmiet@gmail.com>
2026-01-10 16:15:16 +0530

    Accept Changes

--- src/main/java/com/ritesh/javapractice/CollectionFrameworkRelatedProgram/List/MergeTwoLists.java
+++ src/main/java/com/ritesh/javapractice/CollectionFrameworkRelatedProgram/List/MergeTwoLists.java
@@ -1,7 +1,14 @@
 package com.ritesh.javapractice.CollectionFrameworkRelatedProgram.List;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MergeTwoLists {
     public static void main(String[] args) {
-
+        List<String> list1=new ArrayList<>(Arrays.asList("Java","Python","Ruby"));
+        List<String> list2=new ArrayList<>(Arrays.asList("C++","JavaScript"));
+        list1.addAll(list2);
+        System.out.println("Merged List"+list1);
     }
 }
---
+++ src/main/java/com/ritesh/javapractice/CollectionFrameworkRelatedProgram/List/ReturnPeakElementsFromArray.java
@@ -0,0 +1,7 @@
+package com.ritesh.javapractice.CollectionFrameworkRelatedProgram.List;
+
+public class ReturnPeakElementsFromArray {
+    public static void main(String[] args) {
+
+    }
+}
